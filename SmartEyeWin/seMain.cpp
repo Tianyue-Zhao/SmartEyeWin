bfd37163fdb2e185953700a18efc9d446c32ff5b
Tianyue-Zhao <zhaotianyue@outlook.com>
2021-02-20 23:42:33 -0800

    Tested button to trigger function call

--- SmartEyeWin/seMain.cpp
+++ SmartEyeWin/seMain.cpp
@@ -1,9 +1,22 @@
 #include "seMain.h"
+#include "se_set.h"
 
-seMain::seMain() : wxFrame(nullptr, wxID_ANY, "Title")
+wxBEGIN_EVENT_TABLE(seMain, wxFrame)
+	EVT_BUTTON(10001, setLow)
+wxEND_EVENT_TABLE()
+
+seMain::seMain() : wxFrame(nullptr, wxID_ANY, "Title", wxPoint(50,50), wxSize(800,600))
 {
+	testbtn = new wxButton(this, 10001, "TestBTN", wxPoint(10, 10), wxSize(10,10));
 }
 
 seMain::~seMain()
 {
+}
+
+void seMain::setLow(wxCommandEvent& evt)
+{
+	HDC main_context = GetDC(NULL);
+	set_gamma(256, 256, 256, main_context);
+	evt.Skip();
 }
--- SmartEyeWin/seMain.h
+++ SmartEyeWin/seMain.h
@@ -7,5 +7,11 @@ class seMain : public wxFrame
 public:
 	seMain();
 	~seMain();
+
+	wxButton* testbtn = nullptr;
+
+	void setLow(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
 };
 
